d0f6dfacac2cbb1e2d4c88125de16994519cbe69
Danny O Brien <dannyobrien761@gmail.com>
2025-05-25 13:26:44 +0100

    updated database url to postgres

--- marketing_agency/settings.py
+++ marketing_agency/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import environ
 from pathlib import Path
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,11 +120,10 @@ WSGI_APPLICATION = 'marketing_agency.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+
+
 
 
 # Password validation
