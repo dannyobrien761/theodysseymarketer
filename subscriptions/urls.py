b4d4822d101af62b81e98ea0e5011437101b0658
Danny O Brien <dannyobrien761@gmail.com>
2025-05-26 15:40:14 +0100

    changes to subscriptions.views and urls to satisfy webhook endpoint

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.subscriptions_home, name='subscriptions-home'),
     path('pricing/', views.pricing_view, name='pricing'),
     path('checkout/<uuid:plan_id>/', views.create_checkout_session, name='checkout'),
-    path('webhooks/stripe/', subscription_views.stripe_webhook, name='stripe-webhook'),
+    path('webhook/', views.stripe_webhook, name='stripe-webhook'),
 ]
