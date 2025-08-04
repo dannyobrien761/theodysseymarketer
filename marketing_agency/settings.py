9f34c57173931f5f3a45166d638384e2395cceac
Danny O Brien <dannyobrien761@gmail.com>
2025-08-04 17:43:25 +0100

    whitenoise update, debug set to false

--- marketing_agency/settings.py
+++ marketing_agency/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'marketing_agency.urls'
@@ -175,7 +176,7 @@ STATIC_URL = '/static/'
 # gpt static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # gpt Templates
 # TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
 
